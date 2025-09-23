021b0c66faff2db94bf74aaf1352496e883132df
adamkrupa98 <adamkrupa3@gmail.com>
2025-09-23 12:34:52 +0200

    Calcv2

--- day9_project_calc.ts
+++ day9_project_calc.ts
@@ -19,4 +19,53 @@ let calculate = (action: Action, a:number, b:number): number =>{
         case "divide": return divide(a,b);
     }
 }
-console.log(calculate("add",5,5));
+
+
+type Operations = {
+ action: Action|"power",
+ symbol: string,
+ run: (a:number,b:number) => number
+}
+
+let operations: Operations[] = [ {
+    action: "add",
+    symbol: "+",
+    run: (a, b) => a+b
+},
+{
+    action: "substract",
+    symbol: "-",
+    run: (a, b) => a-b
+},
+{
+    action: "multiply",
+    symbol: '*',
+    run: (a, b) => a*b
+},
+{
+    action: "divide",
+    symbol: "/",
+    run: (a, b) => {
+        if (b != 0) {
+            return a/b;
+        } else {
+            throw new Error("blad dzielenia przez 0");
+        }
+    }
+},
+{
+    action: "power",
+    symbol: "^",
+    run: (a,b) => Math.pow(a, b)
+}]
+
+ let calculatev2 = (symbol:string, a:number, b:number):number => {
+    let o = operations.find(o => o.symbol == symbol);
+    if(!o) throw new Error("nie znaleziono operacji");
+    return o.run(a,b);
+ }
+
+ console.log(calculatev2("+",5,10));
+ console.log(calculatev2("-",20,5));
+ console.log(calculatev2("/",10,2));
+ console.log(calculatev2("^",2,4));
